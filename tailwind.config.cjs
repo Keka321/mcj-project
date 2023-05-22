8181b3ffee6d111bb2b76301add3911050afaf25
Stefan <stefan.kekic@vrtuedigital.com>
2023-05-22 11:20:40 +0200

    global css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Museo-Slab': ['museo-slab', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
